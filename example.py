a5e69faf8273ffa1f294e5a6b228601eb1047c5e
Eshwar Andhavarapu <gontadu@gmail.com>
2020-06-08 21:57:12 +0200

    Updated example

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 import oneforge
 
-client = oneforge.Client('your_api_key')
+client = oneforge.Client("your_api_key")
 
-if client.marketIsOpen() == True:
+if client.marketIsOpen() is True:
     print("Market is open!")
 
 print(client.getSymbols())
-print(client.getQuotes(['EUR/USD', 'GBP/JPY']))
+print(client.getQuotes(["EUR/USD", "GBP/JPY"]))
 print(client.getQuota())
-print(client.convert('EUR', 'USD', 100))
+print(client.convert("EUR", "USD", 100))
 
 # End of file
